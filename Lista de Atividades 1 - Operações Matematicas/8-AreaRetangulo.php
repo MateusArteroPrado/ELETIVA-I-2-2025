ba864853e09437b5db8ae2fb30628dd2fc927be1
MateusArteroPrado <mateusartero@gmail.com>
2025-08-22 21:29:34 -0300

    Ex8-Ativ1

---
+++ Lista de Atividades 1 - Operações Matematicas/8-AreaRetangulo.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container">
+<h1>Calcule a área de um retângulo (ou quadrado)!</h1>
+<form method="post">
+<div class="row inline-row mb-3">
+  <div class="col-md-3">
+    <label for="altura" class="form-label">Insira a altura:</label>
+     <input type="number" id="altura" name="altura" class="form-control" required="">
+  </div>
+  <div class="col-md-3">
+    <label for="largura" class="form-label">Insira a largura:</label>
+    <input type="number" id="largura" name="largura" class="form-control" required="">
+  </div>
+  <div class="col-md-3" bis_skin_checked="1">
+    <label for="unidade" class="form-label">unidade</label>
+    <select id="unidade" name="unidade" class="form-select">
+    <option value="centímetros quadrados (cm²)">Centímetros</option>
+    <option value="metros quadrados (m²)">Metros</option>
+    <option value="polegadas quadradas (pol²)">Polegadas</option>
+    </select>
+  </div>
+</div>
+<button type="submit" class="btn btn-primary">Calcular</button>
+</form>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+    {
+    $altura = $_POST['altura'];
+    $largura = $_POST['largura'];
+    $unidade = $_POST['unidade'];
+    $area = $altura * $largura;
+    echo "$altura * $largura = $area $unidade";
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
