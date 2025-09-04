8392e17a18f0cb5a348ab06d33843143241ecda6
MateusArteroPrado <mateusartero@gmail.com>
2025-09-04 20:43:01 -0300

    Ta quase

---
+++ Lista de Atividades 1 - Operações Matematicas/19-Tempo.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container">
+<h1>Convertendo dias em outras unidades de tempo!</h1>
+<form method="post">
+<div class="row inline-row mb-3">
+  <div class="col-md-3">
+    <label for="primeiro" class="form-label">Insira o número de dias:</label>
+     <input type="number" id="primeiro" name="primeiro" class="form-control" required="">
+  </div>
+</div>
+<button type="submit" class="btn btn-primary">Somar</button>
+</form>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+    {
+    $primeiro = $_POST['primeiro'];
+    $horas = $primeiro*24;
+    $minutos = $horas*60;
+    $segundos = $minutos*60;
+    echo "<p>O número de dias inseridos foi: $primeiro</p>";
+    echo "<p>Equivale a $horas horas.</p>";
+    echo "<p>Ou a $minutos minutos.</p>";
+    echo "<p>E também a $segundos segundos.</p>";
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
