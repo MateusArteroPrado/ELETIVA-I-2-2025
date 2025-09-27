b7ef370b71a4f65b0fa3b8ebdc2af3c3922f768e
MateusArteroPrado <mateusartero@gmail.com>
2025-09-27 20:08:01 -0300

    Mexidinha aqui e ali pra diminuir o codigo, mas agora foi

--- Lista de Atividades 3 - Funções internas/Exercicio6.php
+++ Lista de Atividades 3 - Funções internas/Exercicio6.php
@@ -1,11 +1,11 @@
 <?php include('cabecalho.php') ?>
 <div class="container py-3 col-md-8 border ">
-    <h1 class='text-center'>Exercicio 5</h1>
-    <p class='text-center'>Descubra a raiz quadrada de um numero</p>
+    <h1 class='text-center'>Exercicio 6</h1>
+    <p class='text-center'>Insira um número com decimal e receba-o arredondado.</p>
     <form method="post">
         <div class="mb-3">
             <label for="numero" class="form-label">Numero:</label>
-            <input type="number" id="numero" name="numero" class="form-control" required="" placeholder="Insira aqui o numero">
+            <input type="number" id="numero" name="numero" class="form-control" required="" placeholder="Insira aqui o numero" step="any">
         </div>
         <div class="text-center">
             <button type="submit" class="btn btn-success">Enviar</button>
@@ -15,9 +15,9 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $numero = $_POST['numero'];
-    function raizquadrada($numero){
-        echo "<p class='text-center'>A raiz de ".$numero." é ".sqrt($numero).".</p>"; //reduzi o código
+    function arredonda($numero){
+        echo "<p class='text-center'>A versão arredondada de ".$numero." é ".round($numero)."</p>";
     }
-    raizquadrada($numero);
+    arredonda($numero);
 }
 include('rodape.php') ?>
