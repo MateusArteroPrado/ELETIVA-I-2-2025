4b319b0dfd46de107edd9526fdba55934689ac92
MateusArteroPrado <mateusartero@gmail.com>
2025-10-09 22:03:22 -0300

    Ajustezinho

--- projetoAula/cabecalho.php
+++ projetoAula/cabecalho.php
@@ -21,27 +21,27 @@ if (!isset($_SESSION['acesso']))
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="principal.php">Início</a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Cadastros
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="dropdown2">
-                <li><a class="dropdown-item" href="categorias.php">Categorias de Produtos</a></li>
-                <li><a class="dropdown-item" href="produtos.php">Produtos</a></li>
-            </ul>
-        </li>
-    </ul>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="principal.php">Início</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Cadastros
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown2">
+                            <li><a class="dropdown-item" href="categorias.php">Categorias de Produtos</a></li>
+                            <li><a class="dropdown-item" href="produtos.php">Produtos</a></li>
+                        </ul>
+                    </li>
+                </ul>
 
-    <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="logout.php">Logout</a>
-        </li>
-    </ul>
-</div>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </nav>
     <div class="container py-3">
