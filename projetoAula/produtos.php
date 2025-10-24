e0f12606432997a9e3a793d2b718c7d1542cc8d2
MateusArteroPrado <mateusartero@gmail.com>
2025-10-23 21:45:22 -0300

    DESESPERO

--- projetoAula/produtos.php
+++ projetoAula/produtos.php
@@ -1,12 +1,14 @@
 <?php 
 require("cabecalho.php");
 require("conexao.php");
+
 try{
-    $stmt = $pdo->query("SELECT * FROM categoria");
+    $stmt = $pdo->query("SELECT c.nome, p.* FROM produto p INNER JOIN categoria c ON c.id = p.categoria_id");
     $dados = $stmt->fetchAll();
 } catch(\Exception $e){
     echo "Erro: ".$e->getMessage();
 }
+
 if(isset($_GET['cadastro']) && $_GET['cadastro']){
     echo "<p class='text-success'>CADASTRO REALIZADO</p>";
 } else if(isset($_GET['cadastro']) && !$_GET['cadastro']){
@@ -25,13 +27,14 @@ if(isset($_GET['excluir']) && $_GET['excluir']){
 ?>
 
 
-<h2>Categorias</h2>
-<a href="nova_categoria.php" class="btn btn-success mb-3">Novo Registro</a>
+<h2>Produtos</h2>
+<a href="novo_produto.php" class="btn btn-success mb-3">Novo Registro</a>
 <table class="table table-hover table-striped">
     <thead>
         <tr>
             <th>ID</th>
-            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Categoria</th>
             <th>Ações</th>
         </tr>
     </thead>
@@ -39,10 +42,11 @@ if(isset($_GET['excluir']) && $_GET['excluir']){
         <?php foreach($dados as $d): ?>
         <tr>
             <td><?= $d['id'] ?></td>
-            <td><?= $d['nome'] ?></td>
+            <td><?= $d['descricao'] ?></td>
+            <td><?= $d['categoria_id'] ?></td>
             <td class="d-flex gap-2">
-                <a href="editar_categoria.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
-                <a href="consultar_categoria.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-info">Consultar</a>
+                <a href="editar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
+                <a href="consultar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-info">Consultar</a>
             </td>
         </tr>
         <?php endforeach ?>
