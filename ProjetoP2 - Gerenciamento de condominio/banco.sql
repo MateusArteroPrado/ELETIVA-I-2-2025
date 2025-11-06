1a6d1e2b02909ad3136ed0c4e39a7bf4e865982a
MateusArteroPrado <mateusartero@gmail.com>
2025-11-06 19:25:26 -0300

    O start do projeto da P2

---
+++ ProjetoP2 - Gerenciamento de condominio/banco.sql
@@ -0,0 +1,120 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema condominio
+-- -----------------------------------------------------
+
+CREATE SCHEMA IF NOT EXISTS `condominio` DEFAULT CHARACTER SET utf8 ;
+USE `condominio` ;
+
+-- -----------------------------------------------------
+-- Table `condominio`.`unidade`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `condominio`.`unidade` (
+  `id_unidade` INT NOT NULL AUTO_INCREMENT,
+  `numero` INT NOT NULL,
+  `complemento` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`id_unidade`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `condominio`.`morador`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `condominio`.`morador` (
+  `id_morador` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NOT NULL,
+  `unidade_id_unidade` INT NOT NULL,
+  PRIMARY KEY (`id_morador`),
+  INDEX `fk_morador_unidade_idx` (`unidade_id_unidade` ASC)  ,
+  CONSTRAINT `fk_morador_unidade`
+    FOREIGN KEY (`unidade_id_unidade`)
+    REFERENCES `condominio`.`unidade` (`id_unidade`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `condominio`.`veiculo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `condominio`.`veiculo` (
+  `placa` VARCHAR(7) NOT NULL,
+  `modelo` VARCHAR(45) NOT NULL,
+  `cor` VARCHAR(45) NOT NULL,
+  `morador_id_morador` INT NOT NULL,
+  PRIMARY KEY (`placa`),
+  INDEX `fk_veiculo_morador1_idx` (`morador_id_morador` ASC)  ,
+  CONSTRAINT `fk_veiculo_morador1`
+    FOREIGN KEY (`morador_id_morador`)
+    REFERENCES `condominio`.`morador` (`id_morador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `condominio`.`ocorrencia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `condominio`.`ocorrencia` (
+  `id_ocorrencia` INT NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(45) NOT NULL,
+  `descricao` TEXT NOT NULL,
+  `data_hora` DATETIME NOT NULL,
+  `morador_id_morador` INT NOT NULL,
+  PRIMARY KEY (`id_ocorrencia`),
+  INDEX `fk_ocorrencia_morador1_idx` (`morador_id_morador` ASC)  ,
+  CONSTRAINT `fk_ocorrencia_morador1`
+    FOREIGN KEY (`morador_id_morador`)
+    REFERENCES `condominio`.`morador` (`id_morador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `condominio`.`movimentacao`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `condominio`.`movimentacao` (
+  `id_movimentacao` INT NOT NULL AUTO_INCREMENT,
+  `tipo` VARCHAR(45) NOT NULL,
+  `data_hora` DATETIME NOT NULL,
+  `observacao` TEXT NOT NULL,
+  `morador_id_morador` INT NOT NULL,
+  `veiculo_placa` VARCHAR(7) NOT NULL,
+  PRIMARY KEY (`id_movimentacao`),
+  INDEX `fk_movimentacao_morador1_idx` (`morador_id_morador` ASC),
+  INDEX `fk_movimentacao_veiculo1_idx` (`veiculo_placa` ASC),
+  CONSTRAINT `fk_movimentacao_morador1`
+    FOREIGN KEY (`morador_id_morador`)
+    REFERENCES `condominio`.`morador` (`id_morador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_movimentacao_veiculo1`
+    FOREIGN KEY (`veiculo_placa`)
+    REFERENCES `condominio`.`veiculo` (`placa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `condominio`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `condominio`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL,
+  `nome` VARCHAR(255) NOT NULL,
+  `senha` TEXT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
--- projetoAula/produtos.php
+++ projetoAula/produtos.php
@@ -10,32 +10,40 @@ try{
 }
 
 if(isset($_GET['cadastro']) && $_GET['cadastro']){
-    echo "<p class='text-success'>CADASTRO REALIZADO</p>";
+    echo "<p class='text-success no-print'>CADASTRO REALIZADO</p>";
 } else if(isset($_GET['cadastro']) && !$_GET['cadastro']){
-    echo "<p class='text-danger'>ERRO AO CADASTRAR</p>";
+    echo "<p class='text-danger no-print'>ERRO AO CADASTRAR</p>";
 }
 if(isset($_GET['editar']) && $_GET['editar']){
-    echo "<p class='text-success'>CADASTRO EDITADO</p>";
+    echo "<p class='text-success no-print'>CADASTRO EDITADO</p>";
 } else if(isset($_GET['editar']) && !$_GET['editar']){
-    echo "<p class='text-danger'>ERRO AO EDITAR</p>";
+    echo "<p class='text-danger no-print'>ERRO AO EDITAR</p>";
 }
 if(isset($_GET['excluir']) && $_GET['excluir']){
-    echo "<p class='text-success'>CADASTRO EXCLUIDO</p>";
+    echo "<p class='text-success no-print'>CADASTRO EXCLUIDO</p>";
 } else if(isset($_GET['excluir']) && !$_GET['excluir']){
-    echo "<p class='text-danger'>ERRO AO EXCLUIR</p>";
+    echo "<p class='text-danger no-print'>ERRO AO EXCLUIR</p>";
 }
 ?>
 
 
 <h2>Produtos</h2>
-<a href="novo_produto.php" class="btn btn-success mb-3">Novo Registro</a>
+<a href="novo_produto.php" class="btn btn-success mb-3 no-print">Novo Registro</a>
 <table class="table table-hover table-striped">
     <thead>
+        <tr>
+            <th colspan='3'>Dados dos produtos</th>
+            <th class="no-print">
+                <button class='btn btn-secondary' onclick="window.print()">
+                    Imprimir
+                </button>
+            </th>
+        </tr>
         <tr>
             <th>ID</th>
             <th>Descrição</th>
             <th>Categoria</th>
-            <th>Ações</th>
+            <th class="no-print">Ações</th>
         </tr>
     </thead>
     <tbody>
@@ -44,7 +52,7 @@ if(isset($_GET['excluir']) && $_GET['excluir']){
             <td><?= $d['id'] ?></td>
             <td><?= $d['descricao'] ?></td>
             <td><?= $d['nome'] ?></td>
-            <td class="d-flex gap-2">
+            <td class="d-flex gap-2 no-print">
                 <a href="editar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
                 <a href="consultar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-info">Consultar</a>
             </td>
